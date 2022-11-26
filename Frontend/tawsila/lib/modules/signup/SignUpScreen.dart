32c02366fe11b46ac076c961cb8805d6c72bd89b
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2022-11-26 15:41:13 +0200

    alomost sign up done

--- Frontend/tawsila/lib/modules/signup/SignUpScreen.dart
+++ Frontend/tawsila/lib/modules/signup/SignUpScreen.dart
@@ -15,7 +15,7 @@ class SignUpScreen extends StatelessWidget {
   var confirmPasswordController = TextEditingController();
   var cityController = TextEditingController();
   var phoneController = TextEditingController();
-
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     
@@ -26,158 +26,215 @@ class SignUpScreen extends StatelessWidget {
         builder: (context, state) {
           var signUpCubit = SignUpProvider.get(context);
           return Scaffold(
-            body: SingleChildScrollView( 
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 50,
+            body: Directionality(
+              textDirection: signUpCubit.language == "English" ? TextDirection.ltr: TextDirection.rtl,
+              child: Form(
+                key: formKey,
+                child: SingleChildScrollView( 
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10
                     ),
-                    Text(
-                      "${signUpCubit.items['title1']??'error'}",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                        color: Colors.black
-                      ),
-                      ),
-                      Text(
-                      "${signUpCubit.items['title2']??'error'}",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.grey
-                      ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Row(
+                    child: Column(
                       children: [
-                        defaultTextFormFieldRow(
-                          controller: fNameController,
-                          textInputType: TextInputType.name,
-                          validatorFunction: (value) {
-                            if(value.length == 0)
-                              return signUpCubit.items['nameError']??"Error";
-                          },
-                          labelText: signUpCubit.items['Fname']??"Error"
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Text(
+                          "${signUpCubit.items['title1']??'error'}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            color: Colors.black
+                          ),
+                          ),
+                        Text(
+                          "${signUpCubit.items['title2']??'error'}",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.grey
+                          ),
+                          ),
+                        SizedBox(
+                            height: 30,
+                          ),
+                        Row(
+                          children: [
+                            defaultTextFormFieldRow(
+                              controller: fNameController,
+                              textInputType: TextInputType.name,
+                              validatorFunction: (value) {
+                                if(value.length == 0)
+                                  return signUpCubit.items['nameError']??"Error";
+                              },
+                              labelText: signUpCubit.items['Fname']??"Error"
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            defaultTextFormFieldRow(
+                              controller: lNameController,
+                              textInputType: TextInputType.name,
+                              validatorFunction: (value) {
+                                if(value.length == 0)
+                                  return signUpCubit.items['nameError']??"Error";
+                              },
+                              labelText: signUpCubit.items['Lname']??"Error"
+                            ),
+                          ],
                         ),
                         SizedBox(
-                          width: 20,
+                            height: 20,
+                          ),
+                        defaultTextFormFieldColumn(
+                            controller: emailController, 
+                            labelText: signUpCubit.items['email']??"Error", 
+                            validatorFunction: (value) {
+                              if(value.length == 0)
+                                return signUpCubit.items['emailError']??"Error";
+                            }, 
+                            textInputType: TextInputType.emailAddress
+                            ),  
+                        SizedBox(
+                            height: 20,
+                          ),
+                        defaultTextFormFieldColumn(
+                            controller: passwordController, 
+                            labelText: signUpCubit.items['password']??"Error", 
+                            validatorFunction: (value) {
+                              if(value.length == 0)
+                                return signUpCubit.items['passwordError']??"Error";
+                            }, 
+                            textInputType: TextInputType.text,
+                            isSecure: signUpCubit.passwordIsSecure,
+                            suffixIcon: signUpCubit.passwordIsSecure ? Icons.visibility_off : Icons.visibility,
+                            suffixIconFunction: () {
+                              signUpCubit.changePasswordVisibility();
+                            }
+                            ),
+                        SizedBox(
+                            height: 20,
+                          ),
+                        defaultTextFormFieldColumn(
+                            controller: confirmPasswordController,
+                            labelText: signUpCubit.items['confirmPassword']??"Error", 
+                            validatorFunction: (value) {
+                              if(value  != passwordController.text)
+                                return signUpCubit.items['confirmPasswordError']??"Error";
+                            }, 
+                            textInputType: TextInputType.text,
+                            isSecure: signUpCubit.confirmPasswordIsSecure,
+                            suffixIcon: signUpCubit.confirmPasswordIsSecure ? Icons.visibility_off : Icons.visibility,
+                            suffixIconFunction: () {
+                              signUpCubit.changeConfirmPasswordVisibility();
+                            }
+                            ),
+                        SizedBox(
+                          height: 20,
                         ),
-                        defaultTextFormFieldRow(
-                          controller: lNameController,
-                          textInputType: TextInputType.name,
+                        defaultTextFormFieldColumn(
+                          controller: cityController, 
+                          labelText: signUpCubit.items['city']??"Error", 
                           validatorFunction: (value) {
                             if(value.length == 0)
-                              return signUpCubit.items['nameError']??"Error";
+                              return signUpCubit.items['cityError']??"Error";
+                          }, 
+                          textInputType: TextInputType.text
+                          ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        defaultTextFormFieldColumn(
+                          controller: phoneController, 
+                          labelText: signUpCubit.items['phone']??"Error", 
+                          validatorFunction: (value){
+                            //+201021890205
+                            if(value.length != 13)
+                              return signUpCubit.items['phoneError']??"Error";
+                          }, 
+                          textInputType: TextInputType.phone
+                          ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CheckboxListTile(
+                          controlAffinity: ListTileControlAffinity.leading,
+                          title: Text(
+                            "${signUpCubit.items['whatsappCheckBox']??"Error"}"
+                          ),
+                          value: signUpCubit.hasWhatsApp, 
+                          onChanged: (value) {
+                            signUpCubit.hasWhatsAppFun();
                           },
-                          labelText: signUpCubit.items['Lname']??"Error"
+                          ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                child: CheckboxListTile(                         
+                                    controlAffinity: ListTileControlAffinity.leading,
+                                    title: Text(
+                                      "${signUpCubit.items['termsAndConditions']??"Error"}"
+                                    ),
+                                    value: signUpCubit.termsAndConditions, 
+                                    onChanged: (value) {
+                                      signUpCubit.termsAndConditionsFun();
+                                    }, 
+                                    ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                // mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start, 
+                                children: [
+                                  TextButton(
+                                    onPressed: (){
+                                    }, 
+                                    child: Text(
+                                      "${signUpCubit.items['termsAndConditionsPage']??"Error"}",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                      ),
+                                    ),),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),     
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextButton(
+                            child: Text(
+                              "${signUpCubit.items['signUp']??"Error"}",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {
+                              if(formKey.currentState!.validate() && signUpCubit.hasWhatsApp && signUpCubit.termsAndConditions) {
+                                print("ALL Done");
+                              }
+                            },
+                            ),
+                        ),
+                        SizedBox(
+                          height: 10,
                         ),
                       ],
                     ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      defaultTextFormFieldColumn(
-                        controller: emailController, 
-                        labelText: signUpCubit.items['email']??"Error", 
-                        validatorFunction: (value) {
-                          if(value.length == 0)
-                            return signUpCubit.items['emailError']??"Error";
-                        }, 
-                        textInputType: TextInputType.emailAddress
-                        ),  
-                      SizedBox(
-                        height: 20,
-                      ),
-                      defaultTextFormFieldColumn(
-                        controller: passwordController, 
-                        labelText: signUpCubit.items['password']??"Error", 
-                        validatorFunction: (value) {
-                          if(value.length == 0)
-                            return signUpCubit.items['passwordError']??"Error";
-                        }, 
-                        textInputType: TextInputType.text,
-                        isSecure: signUpCubit.passwordIsSecure,
-                        suffixIcon: signUpCubit.passwordIsSecure ? Icons.visibility_off : Icons.visibility,
-                        suffixIconFunction: () {
-                          signUpCubit.changePasswordVisibility();
-                        }
-                        ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      defaultTextFormFieldColumn(
-                        controller: confirmPasswordController,
-                        labelText: signUpCubit.items['confirmPassword']??"Error", 
-                        validatorFunction: (value) {
-                          if(value  != passwordController.text)
-                            return signUpCubit.items['confirmPasswordError']??"Error";
-                        }, 
-                        textInputType: TextInputType.text,
-                        isSecure: signUpCubit.confirmPasswordIsSecure,
-                        suffixIcon: signUpCubit.confirmPasswordIsSecure ? Icons.visibility_off : Icons.visibility,
-                        suffixIconFunction: () {
-                          signUpCubit.changeConfirmPasswordVisibility();
-                        }
-                        ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      defaultTextFormFieldColumn(
-                        controller: cityController, 
-                        labelText: signUpCubit.items['city']??"Error", 
-                        validatorFunction: (value) {
-                          if(value.length == 0)
-                            return signUpCubit.items['cityError']??"Error";
-                        }, 
-                        textInputType: TextInputType.text
-                        ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      defaultTextFormFieldColumn(
-                        controller: phoneController, 
-                        labelText: signUpCubit.items['phone']??"Error", 
-                        validatorFunction: (value){
-                          //+201021890205
-                          if(value.length != 13)
-                            return signUpCubit.items['phoneError']??"Error";
-                        }, 
-                        textInputType: TextInputType.phone
-                        ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      CheckboxListTile(
-                        title: Text(
-                          "${signUpCubit.items['whatsappCheckBox']??"Error"}"
-                        ),
-                        value: signUpCubit.hasWhatsApp, 
-                        onChanged: (value) {
-                          signUpCubit.hasWhatsAppFun();
-                        },
-                        
-                        ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      CheckboxListTile(
-                        title: Text(
-                          "${signUpCubit.items['termsAndConditions']??"Error"}"
-                        ),
-                        value: signUpCubit.termsAndConditions, 
-                        onChanged: (value) {
-                          signUpCubit.termsAndConditionsFun();
-                        }, 
-                        
-                        ),
-                  
-                  ],
+                  ),
                 ),
               ),
             ),
@@ -185,5 +242,4 @@ class SignUpScreen extends StatelessWidget {
       ),
     );
   }
-  
 }
