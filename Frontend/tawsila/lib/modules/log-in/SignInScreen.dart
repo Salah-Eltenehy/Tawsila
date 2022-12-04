2e4669134333e4a7a8634898ce5e88cc872b5bd0
Ahmed Hesham <67082298+Ahmed122000@users.noreply.github.com>
2022-12-04 17:45:04 +0200

    small details
    adding some details

--- Frontend/tawsila/lib/modules/log-in/SignInScreen.dart
+++ Frontend/tawsila/lib/modules/log-in/SignInScreen.dart
@@ -33,80 +33,106 @@ class SignInScreen extends StatelessWidget{
             backgroundColor: Colors.grey[300],  //color of the page background
             body: Directionality(
               textDirection: signUpCubit.language == "English" ? TextDirection.ltr: TextDirection.rtl,  //determine how to start writing (left to right or the opposite) ,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,  //center all the widget in the screen
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:  [
-                        Text(
-                          "${signUpCubit.items['hey']??'error'}",
-                          style:GoogleFonts.lato(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,  //center all the widget in the screen
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:  [
+                          Text(
+                            "${signUpCubit.items['hey']??'error'}",
+                            style:GoogleFonts.lato(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
 
+                            ),
                           ),
+
+                        ],),
+
+                      Text(
+                        "${signUpCubit.items['welcome2']??""}",
+                        style:GoogleFonts.dancingScript(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-
-                      ],),
-
-                    Text(
-                      "login your existing account",
-                      style:GoogleFonts.dancingScript(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
                       ),
-                    ),
 
-                    const SizedBox(height: 10,),
+                      const SizedBox(height: 10,),
 
-                    defaultTextFormFieldColumn(
-                        controller: emailController,
-                        textInputType: TextInputType.emailAddress,
-                        validatorFunction: (value) {
-                          if(!(value.toString().contains('@')))
-                            {return "${signUpCubit.items['emailError']??'error'}";}
-                        },
-                        labelText: "${signUpCubit.items['email']??'error'}"
-                    ),
-
-                    const SizedBox(height: 20,),
-                    defaultTextFormFieldColumn(
-                        controller: passwordController,
-                        labelText: "${signUpCubit.items['password']??'error'}",
-                        validatorFunction: (value, realValue) {
-                          if(value != realValue)
-                            {return "${signUpCubit.items['passwordError']??'error'}";}
-                        },
-                        textInputType: TextInputType.text,
-                        isSecure: signUpCubit.passwordIsSecure,
-                        suffixIcon: signUpCubit.passwordIsSecure ? Icons.visibility_off : Icons.visibility,
-                        suffixIconFunction: () {
-                          signUpCubit.changePasswordVisibility();
-                        }
-                    ),
-
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[900],
-                        borderRadius: BorderRadius.circular(10),
+                      defaultTextFormFieldColumn(
+                          controller: emailController,
+                          textInputType: TextInputType.emailAddress,
+                          validatorFunction: (value) {
+                            if(!(value.toString().contains('@')))
+                              {return "${signUpCubit.items['emailError']??'error'}";}
+                          },
+                          labelText: "${signUpCubit.items['email']??'error'}"
                       ),
-                      child: TextButton(
-                        child: Text(
-                          "${signUpCubit.items['signIn']??"Error"}",
-                          style: const TextStyle(
-                            color: Colors.white,
+
+                      const SizedBox(height: 20,),
+                      defaultTextFormFieldColumn(
+                          controller: passwordController,
+                          labelText: "${signUpCubit.items['password']??'error'}",
+                          validatorFunction: (value, realValue) {
+                            if(value != realValue)
+                              {return "${signUpCubit.items['passwordError']??'error'}";}
+                          },
+                          textInputType: TextInputType.text,
+                          isSecure: signUpCubit.passwordIsSecure,
+                          suffixIcon: signUpCubit.passwordIsSecure ? Icons.visibility_off : Icons.visibility,
+                          suffixIconFunction: () {
+                            signUpCubit.changePasswordVisibility();
+                          }
+                      ),
+
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.blue[900],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextButton(
+                          child: Text(
+                            "${signUpCubit.items['signIn']??"Error"}",
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
                           ),
+                          onPressed: () {print("ALL Done");},
                         ),
-                        onPressed: () {print("ALL Done");},
                       ),
-                    ),
-                  ]
+
+                      const SizedBox(height: 20,),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Row(
+                              children: [
+                                Text(
+                                  "${signUpCubit.items['doNotHaveAnAccount']??""}",
+                                ),
+
+                                TextButton(onPressed: (){},
+                                    child: Text(
+                                        "${signUpCubit.items['registerNow']??""}",
+                                      style: TextStyle(color: Colors.blue),
+                                    ),)
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ]
+                ),
               ),
             ),
           );
