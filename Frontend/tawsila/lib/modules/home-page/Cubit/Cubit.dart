71a69ae104a0f66bc57a125856e7877f75e21326
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2022-12-15 14:29:59 +0200

    fix errors in yaml

--- Frontend/tawsila/lib/modules/home-page/Cubit/Cubit.dart
+++ Frontend/tawsila/lib/modules/home-page/Cubit/Cubit.dart
@@ -11,7 +11,6 @@ import '../../../shared/constants.dart';
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:location/location.dart';
 
--- Frontend/tawsila/lib/modules/signup/cubit/SignUpCubit.dart
+++ Frontend/tawsila/lib/modules/signup/cubit/SignUpCubit.dart
@@ -11,7 +11,6 @@ import 'SignUpStates.dart';
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
