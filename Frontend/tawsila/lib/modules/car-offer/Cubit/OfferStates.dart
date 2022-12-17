b2beb77653850bd2fd573681e4253d3c62c50ef0
Ahmed Hesham <67082298+Ahmed122000@users.noreply.github.com>
2022-12-17 22:19:32 +0200

    Adding Car Offer Screen
    the screen of offering the vehicle for rent I add some text fields and some buttons to take all the details of the car

---
+++ Frontend/tawsila/lib/modules/car-offer/Cubit/OfferStates.dart
@@ -0,0 +1,15 @@
+abstract class OfferStates{}
+
+class OfferInitialState extends OfferStates{}
+
+class TransmissionSelection extends OfferStates{}
+
+class GasSelection extends OfferStates{}
+
+class ChooseOption extends OfferStates{}
+
+class SelectImage extends OfferStates{}
+
+class SetLanguageState extends OfferStates{}
+
+class GetLanguageFromDatabaseState extends OfferStates {}
